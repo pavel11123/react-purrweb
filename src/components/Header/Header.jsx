7ab833211d48dd9332d48f9ad445e03bfd5591fd
pavel11123 <korobko.11@inbox.ru>
2022-09-27 22:25:35 +0600

    add header | Logo + Login

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,21 @@ const Header = () => {
   return (
     <>
       <header className="header">
-        <div className="container"></div>
+        <div className="container">
+          <div className="header__main d-fl">
+            <div className="header__logo d-fl">
+              <div className="header__logo-cube">
+                <img src="./img/icon/logo.png" alt="logo PurrWeb" />
+              </div>
+              <div className="header__logo-name">
+                <img src="./img/icon/logo__notab.svg" alt="logo PurrWeb" />
+              </div>
+            </div>
+            <div className="header__login d-fl">
+              <a href="/">Login</a>
+            </div>
+          </div>
+        </div>
       </header>
     </>
   );
