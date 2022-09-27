17ebddc367cad0235c77f0dd1e2900ae2e98c05b
pavel11123 <korobko.11@inbox.ru>
2022-09-27 20:45:30 +0600

    start first commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import "./Header.scss";
+import React from "react";
+
+const Header = () => {
+  return (
+    <>
+      <header className="header">
+        <div className="container"></div>
+      </header>
+    </>
+  );
+};
+
+export { Header };
---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+import "./Homepage.scss";
+import React from "react";
+
+const Homepage = () => {
+  return (
+    <>
+      <div className="main">
+        <div className="container"></div>
+      </div>
+    </>
+  );
+};
+
+export { Homepage };
