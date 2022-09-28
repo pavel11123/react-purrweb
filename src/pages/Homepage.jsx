54aef09b632423f0efcfeb3b82c4578c875389c0
pavel11123 <korobko.11@inbox.ru>
2022-09-29 00:05:16 +0600

    add adaptive version start | add second section

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -7,7 +7,7 @@ const Homepage = () => {
       <div className="main">
         <div className="container">
           <section className="start__section">
-            <div className="d-fl">
+            <div className="start-main d-fl">
               <div className="start__left d-fl-col">
                 <h1>
                   A platform <br></br> that works smarter
@@ -16,8 +16,8 @@ const Homepage = () => {
                   <div className="start__left-description--line"></div>
                   <p>
                     NoTab allows bars & nightclubs<br></br>to manage,
-                    understand, and grow<br></br>their business from a single
-                    dashboard
+                    understand, and grow their business from
+                    <br className="start__br-none"></br> a single dashboard
                   </p>
                 </div>
               </div>
@@ -30,7 +30,7 @@ const Homepage = () => {
               </div>
             </div>
 
-            <div className="start__bottom d-fl-wrap">
+            <div className="start__bottom d-fl">
               <h2>Download our app</h2>
               <div className="app d-fl">
                 <div className="app__block app__block-store d-fl">
@@ -39,6 +39,18 @@ const Homepage = () => {
                 <div className="app__block app__block-google">
                   <img src="./img/icon/google-play.svg" alt="Google Play" />
                 </div>
+              </div>
+            </div>
+          </section>
+          <section className="info__section">
+            <div className="info-main d-fl-wrap">
+              <div className="info__block d-fl-col">
+                <p>Would like to become a member, please contact us</p>
+                <a href="/">support@notab.com</a>
+              </div>
+              <div className="info__block d-fl-col">
+                <p>Already a partner?</p>
+                <a href="/">Login</a>
               </div>
             </div>
           </section>
