d1a310ee0affac2050ec7f9986dc709daafe910a
pavel11123 <korobko.11@inbox.ru>
2022-09-29 23:56:16 +0600

    add block verification + integration

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -5,8 +5,8 @@ const Homepage = () => {
   return (
     <>
       <div className="main">
-        <div className="container">
-          <section className="start__section">
+        <section className="start__section">
+          <div className="container">
             <div className="start-main d-fl">
               <div className="start__left d-fl-col">
                 <h1>
@@ -29,7 +29,6 @@ const Homepage = () => {
                 </div>
               </div>
             </div>
-
             <div className="start__bottom d-fl">
               <h2>Download our app</h2>
               <div className="app d-fl">
@@ -41,13 +40,15 @@ const Homepage = () => {
                 </div>
               </div>
             </div>
-          </section>
-          <section className="info__section">
+          </div>
+        </section>
+        <section className="info__section">
+          <div className="container">
             <div className="info__main d-fl-wrap">
               <div className="info__main-block d-fl-col">
                 <p>
-                  Would like to become<br></br>a member, please<br></br>contact
-                  us
+                  Would like to become<br></br>a member, please<br></br>
+                  contact us
                 </p>
                 <a href="mailto:support@notab.com">support@notab.com</a>
               </div>
@@ -80,8 +81,136 @@ const Homepage = () => {
                 </div>
               </div>
             </div>
-          </section>
-        </div>
+          </div>
+        </section>
+        <section className="verification__section">
+          <div className="container d-fl-col">
+            <div className="verification__main d-fl">
+              <div className="verification__block verification__block-left d-fl">
+                <div className="verification__phone">
+                  <img
+                    src="./img/photo/verification-1.png"
+                    alt="verification-ID"
+                  />
+                </div>
+                <div className="verification__phone">
+                  <img
+                    src="./img/photo/verification-2.png"
+                    alt="verification-ID"
+                  />
+                </div>
+              </div>
+              <div className="verification__block verification__block-right">
+                <h2 className="title__block">ID Scanner</h2>
+                <p className="text__block">
+                  Scan, verify, and collect data on each person that walks
+                  through your door to humanize your data. You also get a
+                  digital 86 list as well as a citywide ban list to keep your
+                  venue safe.
+                </p>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section className="integration__section">
+          <div className="container">
+            <div className="integration__section-wrapper d-fl">
+              <div className="integration__block integration__block-left">
+                <h2 className="title__block">BI Tool Integration</h2>
+                <p className="text__block">
+                  We use Business Intelligence Tool that allows you to be
+                  updated what is happening in your business just by one click.
+                  Track revenue<br></br>and profit by one click.
+                </p>
+              </div>
+              <div className="integration__block integration__block-right">
+                <div className="diagram diagram-1">
+                  <img
+                    src="./img/photo/diagram-1.svg"
+                    alt="Diagram Bi Tool Integration"
+                  />
+                </div>
+                <div className="diagram diagram-2">
+                  <img
+                    src="./img/photo/diagram-2.svg"
+                    alt="Diagram Bi Tool Integration"
+                  />
+                </div>
+                <div className="diagram diagram-3">
+                  <img
+                    src="./img/photo/diagram-3.svg"
+                    alt="Diagram Bi Tool Integration"
+                  />
+                </div>
+                <div className="diagram diagram-4">
+                  <img
+                    src="./img/photo/diagram-4.svg"
+                    alt="Diagram Bi Tool Integration"
+                  />
+                </div>
+                <div className="diagram diagram-5">
+                  <img
+                    src="./img/photo/diagram-5.svg"
+                    alt="Diagram Bi Tool Integration"
+                  />
+                </div>
+                <div className="d-fl">
+                  <div className="diagram diagram-6">
+                    <img
+                      src="./img/photo/diagram-6.svg"
+                      alt="Diagram Bi Tool Integration"
+                    />
+                  </div>
+                  <div className="diagram diagram-7">
+                    <img
+                      src="./img/photo/diagram-7.svg"
+                      alt="Diagram Bi Tool Integration"
+                    />
+                  </div>
+                  <div className="diagram diagram-8">
+                    <img
+                      src="./img/photo/diagram-8.svg"
+                      alt="Diagram Bi Tool Integration"
+                    />
+                  </div>
+                  <div className="diagram diagram-9">
+                    <img
+                      src="./img/photo/diagram-9.svg"
+                      alt="Diagram Bi Tool Integration"
+                    />
+                  </div>
+                </div>
+
+                <div className="d-fl">
+                  <div className="diagram diagram-10">
+                    <img
+                      src="./img/photo/diagram-10.svg"
+                      alt="Diagram Bi Tool Integration"
+                    />
+                  </div>
+                  <div className="diagram diagram-11">
+                    <img
+                      src="./img/photo/diagram-11.svg"
+                      alt="Diagram Bi Tool Integration"
+                    />
+                  </div>
+                  <div className="diagram diagram-12">
+                    <img
+                      src="./img/photo/diagram-12.svg"
+                      alt="Diagram Bi Tool Integration"
+                    />
+                  </div>
+                  <div className="diagram diagram-13">
+                    <img
+                      src="./img/photo/diagram-13.svg"
+                      alt="Diagram Bi Tool Integration"
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
       </div>
     </>
   );
